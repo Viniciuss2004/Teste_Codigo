76325219955ab8c2f82cedb4840e7ed263d496d2
Viniciuss2004 <viandcustodio@gmail.com>
2025-04-11 00:23:18 -0300

    Fibonacci Teste03 Concluido

--- src/Fibonacci/Teste03.java
+++ src/Fibonacci/Teste03.java
@@ -1,7 +1,26 @@
 package Fibonacci;
 
-public class Teste02 {
+import java.util.Scanner;
+
+public class Teste03 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite uma posição de Fibonacci: ");
+        int n = sc.nextInt();
+
+        System.out.println("\nA posição Fib(" + n + ") é: " + fibonacciLinear(n));
+    }
+
+    public static long fibonacciLinear(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
 }
