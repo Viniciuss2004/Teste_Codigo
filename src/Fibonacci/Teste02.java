fd60eb48af4ee60c76e75f71f85ae8bad1b5ad6f
Viniciuss2004 <viandcustodio@gmail.com>
2025-04-11 00:23:00 -0300

    Fibonacci Teste02 Concluido

--- src/Fibonacci/Teste02.java
+++ src/Fibonacci/Teste02.java
@@ -1,7 +1,19 @@
 package Fibonacci;
 
-public class Teste01 {
+import java.util.Scanner;
+
+public class Teste02 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite uma posição de Fibonacci: ");
+        int n = sc.nextInt();
+
+        System.out.println("\nA posição Fib(" + n + ") é: " + fibonacciRecursiva(n));
+    }
+
+    public static int fibonacciRecursiva(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacciRecursiva(n - 1) + fibonacciRecursiva(n - 2);
     }
 }
